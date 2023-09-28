aa4081f9b953abe8843eef13b092bd6895c750f1
RamiroFerrari <ramiroferrariadrian@gmail.com>
2023-09-28 08:56:19 -0300

    parcial

--- Calculadora_RamiroFerrari/src/Main.java
+++ Calculadora_RamiroFerrari/src/Main.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        //declaracion de variables
         Scanner sc = new Scanner(System.in);
         double num1 = 0;
         double num2 = 0;
         double resultado = 0;
         boolean condicional = true;
-        while (condicional){
+        while (condicional){ //inicio del bucle
+            //cracion del menu de opciones
             System.out.print("bienvenido seleccione A para la suma B para la resta C para multiplicacion D para la division o ingrese E para salir: ");
             String option = sc.next().toLowerCase();
             switch (option) {
-                case "a" -> {
+                case "a" -> {// suma
                     System.out.print("Ingrese el primer numero para la suma: ");
                     num1 = sc.nextDouble();
                     System.out.print("ingrese el segundo numero para la suma: ");
@@ -18,7 +20,7 @@ public class Main {
                     resultado = num1 + num2;
                     System.out.println(resultado);
                 }
-                case "b" -> {
+                case "b" -> {//resta
                     System.out.print("Ingrese el primer numero para la resta: ");
                     num1 = sc.nextDouble();
                     System.out.print("ingrese el segundo numero para la resta: ");
@@ -26,7 +28,7 @@ public class Main {
                     resultado = num1 - num2;
                     System.out.println(resultado);
                 }
-                case "c" -> {
+                case "c" -> {//multiplicacion
                     System.out.print("Ingrese el primer numero para la multiplicacion: ");
                     num1 = sc.nextDouble();
                     System.out.print("ingrese el segundo numero para la multiplicacion: ");
@@ -34,12 +36,12 @@ public class Main {
                     resultado = num1 * num2;
                     System.out.println(resultado);
                 }
-                case "d" -> {
+                case "d" -> {//division
                     System.out.print("Ingrese el primer numero para la division: ");
                     num1 = sc.nextDouble();
                     System.out.print("ingrese el segundo numero para la divicion: ");
                     num2 = sc.nextDouble();
-                    if (num2 == 0){
+                    if (num2 == 0){//division por 0
                         System.out.println("error no se puede dividir por 0");
                         continue;
                     }else {
@@ -48,11 +50,11 @@ public class Main {
                     }
 
                 }
-                case "e" ->{
+                case "e" ->{//salida
                     System.out.println("Adios! vuelva pronto");
                     condicional = false;
                 }
-                default -> {
+                default -> {//opcion incorrecta
                     System.out.println("opcion ingresada incorrecata, vuelva a intentarlo");
                     continue;
                 }
